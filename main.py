841768ec7d6da4ac4b3e48ef9a6149af01272904
rycall13 <rccallahan@live.carlow.edu>
2025-01-26 16:52:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-#Q1
 
-user_name = input("Please enter your name: ")
-
-formatted_name = user_name.title()
-
-greeting_message = f"Hello, {formatted_name}"
-
-print(greeting_message)
-
-with open("q1.out", "w") as file:
-    file.write(greeting_message)
